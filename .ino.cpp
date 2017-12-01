1639cb9e8bbaa2481871fdb9d82de759a9278bee
Pedro Palacios <palmenros@gmail.com>
2017-12-01 17:27:46 +0100

    Initial commit

---
+++ .ino.cpp
@@ -0,0 +1,15 @@
+#ifdef __IN_ECLIPSE__
+//This is a automatic generated file
+//Please do not modify this file
+//If you touch this file your change will be overwritten during the next build
+//This file has been generated on 2017-12-01 17:24:54
+
+#include "Arduino.h"
+#include "Arduino.h"
+void setup() ;
+void loop() ;
+
+#include "Agrotech.ino"
+
+
+#endif
